f0af7dabc8bdfce8a28df2abb01edcda8b9bebf5
giresh03 <135005276+giresh03@users.noreply.github.com>
2025-08-17 21:47:28 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -6,7 +6,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://vit-frontend-50qv.onrender.com",
         methods:["GET","POST"]
     }
 })
@@ -32,6 +32,7 @@ socket.on('disconnect',()=>{
 })
 
 })
+
 
 
 export {app,io, server}
